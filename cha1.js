a5d08fd92c8c7f92452fbbd068f4e4b08c51a524
PJK <PJK@gmail.com>
2023-03-11 17:54:49 +0700

    doPart2

---
+++ cha1.js
@@ -0,0 +1,9 @@
+// Store your birth year in a variable. <br>
+let myBirthYear = 1997
+// Store a future year in another variable. <br>
+let futureYear = 2030
+// Calculate your possible ages for the future year and display it in the console. <br>
+console.log(futureYear-myBirthYear)
+
+
+
---
+++ cha2.js
@@ -0,0 +1,8 @@
+// Create a card holding 3 pieces of information about a person, such as Name, Location, and Hobby.<br>
+// Each piece of information should be a variable.<br>
+let card1 = 'Hi! My name is Kong.'
+let card2 = 'I live in Phayao province, Thailand.'
+let card3 = 'and l like to play Game.'
+// Display the card with the information in the correct place.<br>
+console.log(card1+card2+card3)
+// Example: “Hi, my name is John. I live in South Africa and enjoy drinking homemade wine.”<br>
