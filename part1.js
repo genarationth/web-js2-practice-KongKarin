1ac3f2f7666c67921f37c8eddcd1490d05a042cd
PJK <PJK@gmail.com>
2023-03-12 16:58:41 +0700

    fixedPart1

---
+++ part1.js
@@ -0,0 +1,75 @@
+let a = false;
+console.log("Number : " + Number(a));
+console.log("String : " + String(a));
+console.log("Boolean: " + Boolean(a));
+
+let b = true;
+console.log("Number : " + Number(b));
+console.log("String : " + String(b));
+console.log("Boolean: " + Boolean(b));
+
+let c = 0;
+console.log("Number : " + Number(c));
+console.log("String : " + String(c));
+console.log("Boolean: " + Boolean(c));
+
+let d = 1;
+console.log("Number : " + Number(d));
+console.log("String : " + String(d));
+console.log("Boolean: " + Boolean(d));
+
+let e = "0";
+console.log("Number : " + Number(e));
+console.log("String : " + String(e));
+console.log("Boolean: " + Boolean(e));
+
+let f = "000";
+console.log("Number : " + Number(f));
+console.log("String : " + String(f));
+console.log("Boolean: " + Boolean(f));
+
+let g = "1";
+console.log("Number : " + Number(g));
+console.log("String : " + String(g));
+console.log("Boolean: " + Boolean(g));
+
+let h = NaN;
+console.log("Number : " + Number(h));
+console.log("String : " + String(h));
+console.log("Boolean: " + Boolean(h));
+
+let i = Infinity;
+console.log("Number : " + Number(i));
+console.log("String : " + String(i));
+console.log("Boolean: " + Boolean(i));
+
+let j = -Infinity;
+console.log("Number : " + Number(j));
+console.log("String : " + String(j));
+console.log("Boolean: " + Boolean(j));
+
+let k = "";
+console.log("Number : " + Number(k));
+console.log("String : " + String(k));
+console.log("Boolean: " + Boolean(k));
+
+let l = "20";
+console.log("Number : " + Number(l));
+console.log("String : " + String(l));
+console.log("Boolean: " + Boolean(l));
+
+let m = "Twenty";
+console.log("Number : " + Number(m));
+console.log("String : " + String(m));
+console.log("Boolean: " + Boolean(m));
+
+let o = null;
+console.log("Number : " + Number(o));
+console.log("String : " + String(o));
+console.log("Boolean: " + Boolean(o));
+
+let p = undefined;
+console.log("Number : " + Number(p));
+console.log("String : " + String(p));
+console.log("Boolean: " + Boolean(p));
+
